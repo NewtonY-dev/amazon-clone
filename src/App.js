59d3969f758f236ea561123d34d0fb7b0cf78102
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-03 00:12:18 +0300

    single product

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Test1 from "./Components/Test/Test1";
 import Test2 from "./Components/Test/Test2";
 import CarouselEffect from "./Components/Carousel/CarouselEffect";
 import Category from "./Components/Category/Category";
+import Product from "./Components/Product/Product";
 function App() {
   return (
     <div className="App">
       <Header />
       <CarouselEffect />
       <Category/>
+      <Product/>
     </div>
   );
 }
---
+++ src/Components/Product/ProductCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Rating from "@mui/material/Rating";
+import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
+import classes from './Product.module.css'
+function ProductCard({ product }) {
+  const { id, image, title, price, rating } = product;
+  return (
+    <div className={classes.card_container}>
+      <a href="">
+        <img src={image} alt="" />
+      </a>
+      <div>
+        <h3>{title}</h3>
+        <div className={classes.rating}>
+          {/* rating */}
+          <Rating value={rating.rate} precision={0.1} />
+          {/* count */}
+          <small>{rating.count}</small>
+        </div>
+      </div>
+      <div>
+        {/* price */}
+        <CurrencyFormat amount={price} />
+      </div>
+      <button className={classes.button}>add to cart</button>
+    </div>
+  );
+}
+
+export default ProductCard;
