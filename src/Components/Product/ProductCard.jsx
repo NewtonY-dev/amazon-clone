b46d4607bc757160694c632d5295d7a70e454442
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-07 12:27:11 +0300

    Detail page styling and addition of description on single product

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -2,28 +2,33 @@ import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
-import {Link} from 'react-router'
-function ProductCard({ product }) {
-  const { id, image, title, price, rating } = product;
+import { Link } from "react-router";
+function ProductCard({ product, flex, renderDesc }) {
+  const { id, image, title, price, rating, description } = product;
   return (
-    <div className={classes.card_container}>
+    <div
+      className={`${classes.card_container} ${
+        flex ? classes.product_flexed : ""
+      }`}
+    >
       <Link to={`/products/${id}`}>
         <img src={image} alt="" />
       </Link>
       <div>
         <h3>{title}</h3>
+        {renderDesc && <div style={{ maxWidth: "750px" }}>{description}</div>}
         <div className={classes.rating}>
           {/* rating */}
           <Rating value={rating?.rate || 0} precision={0.1} />
           {/* count */}
           <small>{rating?.count}</small>
         </div>
+        <div className={classes.price}>
+          {/* price */}
+          <CurrencyFormat amount={price} />
+        </div>
+        <button className={classes.button}>add to cart</button>
       </div>
-      <div className={classes.price}>
-        {/* price */}
-        <CurrencyFormat amount={price} />
-      </div>
-      <button className={classes.button}>add to cart</button>
     </div>
   );
 }
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -27,7 +27,11 @@ function ProductDetail() {
   }, []);
   return (
     <LayOut>
-      {isLoading ? <Loader /> : <ProductCard product={product} />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ProductCard product={product} flex={true} renderDesc={true} />
+      )}
     </LayOut>
   );
 }
