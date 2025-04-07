9768d57aac8a8727f5a0b3e8f8919910d3f713d4
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-07 09:40:28 +0300

    Loading Function Integration

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -6,7 +6,7 @@ function Category() {
   return (
     <section className={classes.category_container}>
       {categoryInfos.map((infos) => {
-      return  <CategoryCard key={infos.name} data={infos} />;
+        return <CategoryCard key={infos.name} data={infos} />;
       })}
     </section>
   );
