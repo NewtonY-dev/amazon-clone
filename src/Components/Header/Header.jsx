7d8d37b377f5812dc2a90a85823134cdca163622
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-09 13:56:58 +0300

    Header sticky part implementation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   const [{ basket }, dispatch] = useContext(DataContext);
 
   return (
-    <>
+    <section className={classes.fixed}>
       <section>
         <div className={classes.header_container}>
           {/* logo section*/}
@@ -67,7 +67,7 @@ function Header() {
         </div>
       </section>
       <LowerHeader />
-    </>
+    </section>
   );
 }
 
