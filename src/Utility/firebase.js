e4f72bc4c77a8edf08f65336d55bdc9a5758798f
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-13 22:05:14 +0300

    Firebase  Setup

---
+++ src/Utility/firebase.js
@@ -0,0 +1,30 @@
+import firebase from "firebase/compat/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const APP_ID = process.env.REACT_APP_APP_ID;
+const MEASUREMENT_ID = process.env.REACT_APP_MEASUREMENT_ID;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = app.firestore();
